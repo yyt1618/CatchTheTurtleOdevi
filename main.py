4dd44c0948cf34309cb55e4a1646a523f0910534
Yasin Yakup Taşpınar <yyt1.618@gmail.com>
2024-06-25 15:16:02 +0300

    set cursor shape and color

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ drawing_board.bgcolor("light blue")
 drawing_board.title("Catch The Turtle")
 drawing_board.setup(width=600, height=600)
 my_turtle = turtle.Turtle()
+
+# kursörümüzü yeşil kaplumbağa yapalım:
+my_turtle.shape("turtle")
+my_turtle.color("green")
