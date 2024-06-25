98b7184535ab2a0f701a323bacc6d216b3144db5
Yasin Yakup Taşpınar <yyt1.618@gmail.com>
2024-06-25 16:15:15 +0300

    move_randomly() added

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ drawing_board = turtle.Screen()
 drawing_board.bgcolor("light blue")
 drawing_board.title("Catch The Turtle")
 drawing_board.setup(width=600, height=600)
-my_turtle = turtle.Turtle()
 
 # kursörümüzü yeşil kaplumbağa yapalım:
+my_turtle = turtle.Turtle()
 my_turtle.shape("turtle")
 my_turtle.color("green")
-
-# kalemi kaldıralım:
 my_turtle.penup()
 
 # zaman sayacını gösterecek turtle'ı oluştur
@@ -36,6 +34,16 @@ def countdown(time_left):
         turtle.hideturtle()
         my_turtle.hideturtle()
 
+# turtle'ı rastgele hareket ettir:
+def move_randomly():
+    if start_time > 0:
+        x = random.randint(-300, 300)
+        y = random.randint(-300, 300)
+        my_turtle.goto(x, y)
+        drawing_board.ontimer(move_randomly, 400)
+
 countdown(start_time)
+
+move_randomly()
 
 turtle.mainloop()
