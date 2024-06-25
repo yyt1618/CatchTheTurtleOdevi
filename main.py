425584fdb2054f42cde887e0094c1c8b1be72566
Yasin Yakup Taşpınar <yyt1.618@gmail.com>
2024-06-25 15:44:48 +0300

    timer added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+import time
 
 drawing_board = turtle.Screen()
 drawing_board.bgcolor("light blue")
@@ -13,5 +14,28 @@ my_turtle.color("green")
 
 # kalemi kaldıralım:
 my_turtle.penup()
+
+# zaman sayacını gösterecek turtle'ı oluştur
+counter_turtle = turtle.Turtle()
+counter_turtle.color("black")
+counter_turtle.hideturtle()
+counter_turtle.penup()
+counter_turtle.goto(0, 200)
+
+start_time = 20
+
+# zamanı güncelle ve ekranda göster:
+def countdown(time_left):
+    if time_left > 0:
+        counter_turtle.clear()
+        counter_turtle.write(time_left, align="center", font=("Arial", 24, "normal"))
+        drawing_board.ontimer(lambda: countdown(time_left - 1), 1000)
+    else:
+        counter_turtle.clear()
+        counter_turtle.write("Game Over!", align="center", font=("Arial", 24, "normal"))
+        turtle.hideturtle()
+        my_turtle.hideturtle()
+
+countdown(start_time)
 
 turtle.mainloop()
