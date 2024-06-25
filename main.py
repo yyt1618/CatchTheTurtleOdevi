015686a819b44b15e487e821584a0e9741f5566a
Yasin Yakup Taşpınar <yyt1.618@gmail.com>
2024-06-25 15:13:42 +0300

    window and turtle created

---
+++ main.py
@@ -0,0 +1,8 @@
+import turtle
+import random
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("light blue")
+drawing_board.title("Catch The Turtle")
+drawing_board.setup(width=600, height=600)
+my_turtle = turtle.Turtle()
