e5f7acb872d404f0d42ae04bf09feed256f89a86
Yasin Yakup Taşpınar <yyt1.618@gmail.com>
2024-06-25 15:16:59 +0300

    pen removed and mainloop added

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ my_turtle = turtle.Turtle()
 # kursörümüzü yeşil kaplumbağa yapalım:
 my_turtle.shape("turtle")
 my_turtle.color("green")
+
+# kalemi kaldıralım:
+my_turtle.penup()
+
+turtle.mainloop()
